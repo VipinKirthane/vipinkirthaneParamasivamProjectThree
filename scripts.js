3df5ee74377b9928b2aadb0e151735fe8e5e79aa
Vipin <kirthane.vipin@gmail.com>
2019-11-13 16:28:33 -0500

    Added Wager  mechanics

--- scripts.js
+++ scripts.js
@@ -1,16 +1,41 @@
 
 
 $(function () {
-    let randomNumberImage = (Math.floor(Math.random() * 3) + 1);
+
     
+    // Random Image Section
+    const randomNumGenerator = function (){
+        return (Math.floor(Math.random() * 3) + 1);
+    };
     
-    $('.slotImage'+randomNumberImage).show();
-    // let slotImage = 
+    let randomNumberImage1 = randomNumGenerator();
+    let randomNumberImage2 = randomNumGenerator();
+    let randomNumberImage3 = randomNumGenerator();
+    
+    $('button').on('click', function(){
+        $('.slotMachine1 .slotImage' + randomNumberImage1).show();
+        $('.slotMachine2 .slotImage' + randomNumberImage2).show();
+        $('.slotMachine3 .slotImage' + randomNumberImage3).show();
+    })
+    
+    if ( randomNumberImage1 === randomNumberImage2 === randomNumberImage3) {
+    
+    }
 
 
+    // Player Cash
+    let playerCash = 1000;
+    let defaultWager = 100;
 
+    if (randomNumberImage1 === randomNumberImage2 === randomNumberImage3) {
+        playerCash = ((playerCash-100) + (defaultWager += defaultWager));
+    } else if (randomNumberImage1 === randomNumberImage2 || randomNumberImage2 === randomNumberImage3 || randomNumberImage3 === randomNumberImage1) {
+        playerCash = ((playerCash-100) + (defaultWager + (defaultWager / 2)));
+    } else {
+        playerCash = ((playerCash-100) - (defaultWager - defaultWager));
+    }
 
-
+    console.log(playerCash);
 
 
 })
