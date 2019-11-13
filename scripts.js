3d36b37e41797af4975a01bc9f15751a8dc31889
Vipin <kirthane.vipin@gmail.com>
2019-11-13 10:06:45 -0500

    Added more stylings & jQuery code

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+$(function () {
+    let randomNumberImage = (Math.floor(Math.random() * 3) + 1);
+    
+    
+    $('.slotImage'+randomNumberImage).show();
+    // let slotImage = 
+
+
+
+
+
+
+
+})
