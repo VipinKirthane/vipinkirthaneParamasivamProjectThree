ba9094e1ad2823c1e0faad36508ddd90e99a6536
Vipin <kirthane.vipin@gmail.com>
2019-11-13 23:13:32 -0500

    Added more functionality

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@
 $(function () {
 
     
-    // Random Image Section
-    const randomNumGenerator = function (){
+    //------------ Random Image Chooser------------------
+    const randomNumGenerator = function () {
         return (Math.floor(Math.random() * 3) + 1);
     };
     
@@ -12,30 +12,52 @@ $(function () {
     let randomNumberImage2 = randomNumGenerator();
     let randomNumberImage3 = randomNumGenerator();
     
+    // console.log(randomNumberImage1);
+    // console.log(randomNumberImage2);
+    // console.log(randomNumberImage3);
+
     $('button').on('click', function(){
-        $('.slotMachine1 .slotImage' + randomNumberImage1).show();
-        $('.slotMachine2 .slotImage' + randomNumberImage2).show();
-        $('.slotMachine3 .slotImage' + randomNumberImage3).show();
+        //Hide all results
+        $('.slot1').html(`<img src="assets/image${randomNumberImage1}.png" alt="">`);
+        $('.slot2').html(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
+        $('.slot3').html(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
+        // $('.slotMachine2 .slotImage' + randomNumberImage2).append(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
+        // $('.slotMachine3 .slotImage' + randomNumberImage3).append(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
+        
+        combinationChecker();
+
+        // FOR CHECKING IF THE PLAYER HAS ENOUGH MONEY TO CONTINUE THE GAME
+        const balanceCashChecker = () => {
+            if (playerCash >= 100) {
+                // continue game
+                $('.slot1').html(`<img src="assets/image${randomNumberImage1}.png" alt="">`);
+                $('.slot2').html(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
+                $('.slot3').html(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
+            } else if (playerCash >= 2000) {
+                // you are cheating, get out of the casino
+            } else {
+                // No more money? SECURITY!!!!
+            }
+        }
+
+        //Adjust score
     })
+    //------------ Random Image Chooser-----------------
     
-    if ( randomNumberImage1 === randomNumberImage2 === randomNumberImage3) {
-    
-    }
 
-
-    // Player Cash
+    //------------ Player Cash Counter------------------
     let playerCash = 1000;
     let defaultWager = 100;
-
-    if (randomNumberImage1 === randomNumberImage2 === randomNumberImage3) {
-        playerCash = ((playerCash-100) + (defaultWager += defaultWager));
-    } else if (randomNumberImage1 === randomNumberImage2 || randomNumberImage2 === randomNumberImage3 || randomNumberImage3 === randomNumberImage1) {
-        playerCash = ((playerCash-100) + (defaultWager + (defaultWager / 2)));
-    } else {
-        playerCash = ((playerCash-100) - (defaultWager - defaultWager));
+    
+    const combinationChecker = () => {
+        if (randomNumberImage1 === randomNumberImage2 && randomNumberImage2 === randomNumberImage3) {
+            playerCash = ((playerCash - defaultWager) + (defaultWager * 2));
+        } else if (randomNumberImage1 === randomNumberImage2 || randomNumberImage2 === randomNumberImage3 || randomNumberImage3 === randomNumberImage1) {
+            playerCash = ((playerCash - defaultWager) + (defaultWager + (defaultWager / 2)));
+        } else if (randomNumberImage1 != randomNumberImage2 && randomNumberImage2 != randomNumberImage3) {
+            playerCash = ((playerCash - defaultWager));
+        }
+        console.log(playerCash);
     }
-
-    console.log(playerCash);
-
-
 })
+//------------ Player Cash Counter-----------------------
