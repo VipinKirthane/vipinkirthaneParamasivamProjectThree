9998ad4a0ecaf448a1ecd4e5d00e06cab99fd8ea
Vipin <kirthane.vipin@gmail.com>
2019-11-14 15:39:41 -0500

    Added Spin reset function

--- scripts.js
+++ scripts.js
@@ -3,9 +3,6 @@
 $(function () {
 
     let roundNumber = 0;
-    
-
-    
     //------------ Random Image Chooser------------------
     const randomNumGenerator = function () {
         return (Math.floor(Math.random() * 3) + 1);
@@ -15,37 +12,28 @@ $(function () {
     let randomNumberImage2 = randomNumGenerator();
     let randomNumberImage3 = randomNumGenerator();
     
-    // console.log(randomNumberImage1);
-    // console.log(randomNumberImage2);
-    // console.log(randomNumberImage3);
-
     $('button').on('click', function(){
-        //Hide all results
+        randomNumberImage1 = randomNumGenerator();
+        randomNumberImage2 = randomNumGenerator();
+        randomNumberImage3 = randomNumGenerator();
+
         $('.slot1').html(`<img src="assets/image${randomNumberImage1}.png" alt="">`);
         $('.slot2').html(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
         $('.slot3').html(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
-        // $('.slotMachine2 .slotImage' + randomNumberImage2).append(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
-        // $('.slotMachine3 .slotImage' + randomNumberImage3).append(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
         
         combinationChecker();
         showPlayerStats();
         
-        
-
         // FOR CHECKING IF THE PLAYER HAS ENOUGH MONEY TO CONTINUE THE GAME
         const balanceCashChecker = () => {
             if (playerCash >= 100) {
                 // continue game
-                $('.slot1').html(`<img src="assets/image${randomNumberImage1}.png" alt="">`);
-                $('.slot2').html(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
-                $('.slot3').html(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
             } else if (playerCash >= 2000) {
                 // you are cheating, get out of the casino
             } else {
                 // No more money? SECURITY!!!!
             }
         }
-
         //Adjust score
            
     })
@@ -69,8 +57,6 @@ $(function () {
         }
     }
 
-    
     showPlayerStats();
-    
 })
 //------------ Player Cash Counter-----------------------
