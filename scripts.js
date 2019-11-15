715f9dfe8e860c539f2271eb848605992f685e1d
Vipin <kirthane.vipin@gmail.com>
2019-11-14 23:46:10 -0500

    Added animations to some elements

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@
 $(function () {
 
     let roundNumber = 0;
-    //------------ Random Image Chooser------------------
+    let winnings = 0;
+    //-------------------- Random Image Chooser-----------------------------
     const randomNumGenerator = function () {
         return (Math.floor(Math.random() * 3) + 1);
     };
@@ -12,7 +13,7 @@ $(function () {
     let randomNumberImage2 = randomNumGenerator();
     let randomNumberImage3 = randomNumGenerator();
     
-    $('button').on('click', function(){
+    const playGame = () => {
         randomNumberImage1 = randomNumGenerator();
         randomNumberImage2 = randomNumGenerator();
         randomNumberImage3 = randomNumGenerator();
@@ -20,43 +21,68 @@ $(function () {
         $('.slot1').html(`<img src="assets/image${randomNumberImage1}.png" alt="">`);
         $('.slot2').html(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
         $('.slot3').html(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
-        
+
         combinationChecker();
         showPlayerStats();
+        balanceCashChecker();
+    }
+
+    $('button').on('click', function(){
+        playGame();
+        $('button').addClass('animated bounce');
         
-        // FOR CHECKING IF THE PLAYER HAS ENOUGH MONEY TO CONTINUE THE GAME
-        const balanceCashChecker = () => {
-            if (playerCash >= 100) {
-                // continue game
-            } else if (playerCash >= 2000) {
-                // you are cheating, get out of the casino
-            } else {
-                // No more money? SECURITY!!!!
-            }
-        }
         //Adjust score
            
     })
-    //------------ Random Image Chooser-----------------
+    //---------------------- Random Image Chooser--------------------------
     
+
+    //------------------To check if player has enough cash-------------------
+    const balanceCashChecker = () => {
+        if (playerCash > 100 && playerCash < 2000) {
+            // continue game
+        } else if (playerCash >= 2000) {
+            // you are cheating, get out of the casino
+            alert('get out');
+        } else {
+            // No more money? SECURITY!!!!
+        }
+    }
+//------------------To check if player has enough cash-------------------
+    
+
+//------------------ Shows Player Stats ---------------------------------
     const showPlayerStats = () => {
         $('.playerCash').text('Cash Pile: $' + playerCash);
         $('.roundNumber').text(`Round Number: ${roundNumber++}`);
+        $('.winnings').html(`Winnings: ${winnings}`);
     }
-    //------------ Player Cash Counter------------------
+//------------------ Shows Player Stats ---------------------------------
+
+
+//------------------------- Player Cash Counter----------------------------
     let playerCash = 1000;
     let defaultWager = 100;
     
     const combinationChecker = () => {
         if (randomNumberImage1 === randomNumberImage2 && randomNumberImage2 === randomNumberImage3) {
             playerCash = ((playerCash) + (defaultWager * 2));
+            winnings = 200;
         } else if (randomNumberImage1 === randomNumberImage2 || randomNumberImage2 === randomNumberImage3 || randomNumberImage3 === randomNumberImage1) {
             playerCash = ((playerCash + (defaultWager / 2)));
+            winnings =  50;
         } else if (randomNumberImage1 != randomNumberImage2 && randomNumberImage2 != randomNumberImage3) {
             playerCash = ((playerCash - defaultWager));
+            winnings = -100;
         }
     }
 
     showPlayerStats();
+
+    const winningsStatus = () => {
+        
+    }
+
+    winningsStatus();
 })
-//------------ Player Cash Counter-----------------------
+//---------------------------- Player Cash Counter----------------------------
