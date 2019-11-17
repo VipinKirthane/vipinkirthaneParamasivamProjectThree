766c9286af08ecd6e6e353c9948e352faf52921a
Vipin <kirthane.vipin@gmail.com>
2019-11-16 23:00:38 -0500

    Added fonts & lots of CSS modifications

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-
+let roundNumber = 0;
+let winnings = 0;
 
 $(function () {
 
-    let roundNumber = 0;
-    let winnings = 0;
+
     //-------------------- Random Image Chooser-----------------------------
     const randomNumGenerator = function () {
         return (Math.floor(Math.random() * 3) + 1);
@@ -12,7 +12,11 @@ $(function () {
     let randomNumberImage1 = randomNumGenerator();
     let randomNumberImage2 = randomNumGenerator();
     let randomNumberImage3 = randomNumGenerator();
-    
+    //---------------------- Random Image Chooser--------------------------
+
+
+
+    // -------------------- Play Game Function ----------------------------
     const playGame = () => {
         randomNumberImage1 = randomNumGenerator();
         randomNumberImage2 = randomNumGenerator();
@@ -21,46 +25,50 @@ $(function () {
         $('.slot1').html(`<img src="assets/image${randomNumberImage1}.png" alt="">`);
         $('.slot2').html(`<img src="assets/image${randomNumberImage2}.png" alt="">`);
         $('.slot3').html(`<img src="assets/image${randomNumberImage3}.png" alt="">`);
-
+        roundNumber++;
         combinationChecker();
         showPlayerStats();
         balanceCashChecker();
     }
+    // -------------------- Play Game Function ----------------------------
 
-    $('button').on('click', function(){
-        playGame();
-        $('button').addClass('animated bounce');
-        
-        //Adjust score
-           
-    })
-    //---------------------- Random Image Chooser--------------------------
-    
+
 
     //------------------To check if player has enough cash-------------------
     const balanceCashChecker = () => {
-        if (playerCash > 100 && playerCash < 2000) {
+        if (playerCash > 10000 && playerCash < 200000) {
             // continue game
-        } else if (playerCash >= 2000) {
+        } else if (playerCash >= 1100) {
             // you are cheating, get out of the casino
-            alert('get out');
+            playerCash=1000;
+            winnings=0;
+            roundNumber=0;
+            $('.playerCash').text(`Cash Pile: $${playerCash}`);
+            $('.winnings').html(`Winnings: $${winnings}`);
+            // alert('You are cheating!!!, get out');
+            $('.messageDialog').text('You lose!!').css('display','inline-block');
+            $('.roundNumber').text(`Round Number: ${roundNumber}`);
         } else {
-            // No more money? SECURITY!!!!
+            // alert('Get outta here, you are broke now!')
         }
     }
-//------------------To check if player has enough cash-------------------
+    //------------------To check if player has enough cash-------------------
     
 
-//------------------ Shows Player Stats ---------------------------------
+
+
+    //--------------------- Shows Player Stats ------------------------------
     const showPlayerStats = () => {
-        $('.playerCash').text('Cash Pile: $' + playerCash);
-        $('.roundNumber').text(`Round Number: ${roundNumber++}`);
-        $('.winnings').html(`Winnings: ${winnings}`);
+        $('.playerCash').text(`Cash Pile: $${playerCash}`);
+        $('.roundNumber').text(`Round Number: ${roundNumber}`);
+        $('.winnings').html(`Winnings: $${winnings}`);
     }
-//------------------ Shows Player Stats ---------------------------------
+    //----------------------- Shows Player Stats -----------------------------
 
 
-//------------------------- Player Cash Counter----------------------------
+
+
+    //------------------------- Player Cash Counter----------------------------
     let playerCash = 1000;
     let defaultWager = 100;
     
@@ -77,12 +85,21 @@ $(function () {
         }
     }
 
+    //------------------------- Player Cash Counter----------------------------
+    
+
+
+
+
+    //---------------------- Click Event to START the game ----------------
+    $('button').on('click', function () {
+        playGame();
+        // Using slideup() to create delay for bounce effect to happen
+        $('.winnings').removeClass('zoomIn').removeClass('heartBeat').slideUp(1).slideDown(1).addClass('heartBeat');
+        $('.startButton').removeClass('zoomIn').removeClass('pulse').slideUp(1).slideDown(1).addClass('pulse');
+        $('img').removeClass('slideInUp').removeClass('slideInUp').slideUp(1).slideDown(1).addClass('slideInUp');
+    })
+    //---------------------- Click Event to START the game ----------------
     showPlayerStats();
-
-    const winningsStatus = () => {
-        
-    }
-
-    winningsStatus();
 })
 //---------------------------- Player Cash Counter----------------------------
